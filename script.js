3ebd825a60b331e557eb07c0e0f6c60a46ab3b66
Stephanie <stephanie@fonk.amsterdam>
2021-07-09 14:04:01 +0200

    Border radius slider works and changes the radius of the div

--- script.js
+++ script.js
@@ -4,23 +4,13 @@ const blob = document.getElementById("blob");
 const submitButton = document.getElementById("sumbit");
 
 const borderRadius = document.getElementById("border-radius");
-const showBorderRadiusValue = document.getElementById("border-radius-value");
-
+const copyCodeButton = document.getElementById("copyCode");
 
 
 
 borderRadius.oninput = function () {
-    showBorderRadiusValue.innerHTML = borderRadius.value;
+    blob.style.borderRadius = `${borderRadius.value}px`;
 }
 
 
-const borderRadiusValue = "80px";
-
-
-function changeValue(e) {
-    e.preventDefault();
-    blob.style.borderRadius = borderRadiusValue;
-}
-
-submitButton.addEventListener('click', changeValue);
 
