428454ebf04936e1deceff7d3fb6505ec2f8ae0c
Stephanie <stephanie@fonk.amsterdam>
2021-07-09 15:34:11 +0200

    code is copied to clipbord after pressing button

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 console.log(`The JS works!`);
 
 const blob = document.getElementById("blob");
-const submitButton = document.getElementById("sumbit");
-
 const borderRadius = document.getElementById("border-radius");
+const color = document.getElementById("color");
 const copyCodeButton = document.getElementById("copyCode");
 
 
 
+//---border
+//function that changes the blob after the user changed the value of the slider
 borderRadius.oninput = function () {
     blob.style.borderRadius = `${borderRadius.value}px`;
-}
+};
 
 
-
-//function that copies the css code to the clipboard
-function copyCode() {
-    console.log(`copy code`);
+//function that creates the code based on the user input.
+function createCode() {
     code = `
     #blob{
       width: 10rem;
@@ -25,7 +24,44 @@ function copyCode() {
       border-radius: ${borderRadius.value}px;
     }
     `;
-    console.log(code);
+};
+
+
+//function that copies the code
+function copyCode() {
+
+    createCode();
+
+    this.copied = false
+
+    // Create textarea element
+    const textarea = document.createElement('textarea')
+
+    // Set the value of the text
+    textarea.value = code;
+
+    // Make sure we cant change the text of the textarea
+    textarea.setAttribute('readonly', '');
+
+    // Hide the textarea off the screnn
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
+
+    // Add the textarea to the page
+    document.body.appendChild(textarea);
+
+    // Copy the textarea
+    textarea.select()
+
+    try {
+        var successful = document.execCommand('copy');
+        this.copied = true
+    } catch (err) {
+        this.copied = false
+    };
+
+    textarea.remove()
+
 };
 
 copyCodeButton.addEventListener('click', copyCode);
