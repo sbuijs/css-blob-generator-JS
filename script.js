49f410b105940022c5f4edd025e3ae2d545099b7
Stephanie <stephanie@fonk.amsterdam>
2021-07-09 13:52:57 +0200

    added sliders for color and border radius. Slider works and showing iput value on a p element

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log(`The JS works!`);
+
+const blob = document.getElementById("blob");
+const submitButton = document.getElementById("sumbit");
+
+const borderRadius = document.getElementById("border-radius");
+const showBorderRadiusValue = document.getElementById("border-radius-value");
+
+
+
+
+borderRadius.oninput = function () {
+    showBorderRadiusValue.innerHTML = borderRadius.value;
+}
+
+
+const borderRadiusValue = "80px";
+
+
+function changeValue(e) {
+    e.preventDefault();
+    blob.style.borderRadius = borderRadiusValue;
+}
+
+submitButton.addEventListener('click', changeValue);
+
